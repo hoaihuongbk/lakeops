c8a785526f45fc832f77f42325654e88cdf5b9a9
hoaihuongbk <hoaihuongvuonghuynh@gmail.com>
2025-02-09 14:50:37 +0800

    Fix trino related import inside init class

--- src/lakeops/core/engines/trino.py
+++ src/lakeops/core/engines/trino.py
@@ -2,9 +2,6 @@ from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 import polars as pl
-from sqlalchemy import create_engine
-from trino.auth import BasicAuthentication
-
 from .base import Engine
 
 
@@ -21,6 +18,8 @@ class TrinoEngineConfig:
     def connection(self):
         # https://sfu-db.github.io/connector-x/databases/trino.html
         # https://github.com/trinodb/trino-python-client
+        from trino.auth import BasicAuthentication
+        from sqlalchemy import create_engine
 
         auth = None
         if self.password:
