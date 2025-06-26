b88df7d20b3c1d35c0fb36666f1c83a01b33f25b
hoaihuongbk <hoaihuongvuonghuynh@gmail.com>
2025-06-26 22:42:15 +0800

    Add SparkConnectEngine and test cases

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,12 +7,13 @@ use json::functions::{json_extract, is_valid_json};
 
 #[pymodule]
 #[pyo3(name="_lakeops_udf")]
-fn lakeops_udf(_py: Python, m: &PyModule) -> PyResult<()> {
+fn lakeops_udf(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(point_in_polygon, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_distance, m)?)?;
     m.add_function(wrap_pyfunction!(json_extract, m)?)?;
     m.add_function(wrap_pyfunction!(is_valid_json, m)?)?;
     m.add_function(wrap_pyfunction!(encode_geohash, m)?)?;
     m.add_function(wrap_pyfunction!(decode_geohash, m)?)?;
+
     Ok(())
 }
